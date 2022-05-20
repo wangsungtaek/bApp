ae6d73504e2b66fb36b0261836c861b7e35f0bd2
wangsungtaek <tjdxorv@naver.com>
2022-05-20 23:53:27 +0900

    기본 스마트컨트랙트 기능 구현

--- practice_2.sol
+++ practice_2.sol
@@ -7,19 +7,50 @@ contract Practice {
     mapping (uint256 => address) public tokenOwner;
     mapping (uint256 => string) public tokenURIs;
 
+    // 소유한 토큰 리스트
+    mapping (address => uint256[]) private _ownedTokens;
+
     // mint(tokenId, uti, owner)
     // transferFrom (from, to, tokenId)
 
-    function mintWithTokenURI(address to, uint256 tokenId, string memory tokenURI) public {
+    function mintWithTokenURI(address to, uint256 tokenId, string memory tokenURI) public returns (bool) {
         tokenOwner[tokenId] = to;
         tokenURIs[tokenId] = tokenURI;
+
+        // add token to the list
+        _ownedTokens[to].push(tokenId);
+
+        return true;
     }
 
     function safeTransferFrom(address from, address to, uint256 tokenId) public {
         require(from == msg.sender, "from != msg.sender");
         require(from == tokenOwner[tokenId], "you are not the owner of th token");
 
+        _removeTokenFromList(from, tokenId);
+        _ownedTokens[to].push(tokenId);
+
         tokenOwner[tokenId] = to;
+    }
+
+    function _removeTokenFromList(address from, uint256 tokenId) private {
+        // [10, 15, 19, 20] -> 19번을 삭제 하고 싶으면
+        // [10, 15, 20, 19] -> 자리를 변경한다.
+        // [10, 15, 20] -> 길이를 자른다.
+        uint256 lastTokenIndex = _ownedTokens[from].length - 1;
+        for(uint256 i=0; i<_ownedTokens[from].length; i++) {
+            if(tokenId == _ownedTokens[from][i]) {
+                _ownedTokens[from][i] = _ownedTokens[from][lastTokenIndex];
+                _ownedTokens[from][lastTokenIndex] = tokenId;
+                break;
+            }
+        }
+        _ownedTokens[from].length--;
+    }
+
+    // 해당 주소가 보유한 토큰 ID 확인
+    function ownedTokens(address owner) public view returns (uint256[] memory) {
+        return _ownedTokens[owner];
     }
 
     function setTokenUri(uint256 id, string memory uri) public {
